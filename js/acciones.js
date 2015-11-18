4e698bf628c5c796bb4ebea705694cbfcda77f2b
DelfinoRojas <damcb164e32@gmail.com>
2015-11-18 11:58:09 -0600

    4 subida
    4 intento

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,22 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	function consulta()
+	{
+		datos="";
+		$.ajax({
+			type: "POST",
+			url: "192.168.1.30/procesar.php",
+			data: datos
+		}).done(function(mensaje){
+			alert(mensaje);
+		});
+	}
+$("#btnconsulta").on("tap", function(){
+	alert("Preguntando...");
+	consulta();
 	
+});
 }); 
 });
 
